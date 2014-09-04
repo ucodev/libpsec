98a0ac87c60f578486b1c6bc0a64f8c728b4f52e
Pedro A. Hortas <pah@ucodev.org>
2014-09-05 00:59:01 +0100

    v0.06d

---
+++ example/kdf_pbkdf2_md5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <psec/encode.h>
+#include <psec/hash.h>
+#include <psec/kdf.h>
+#include <psec/mac.h>
+
+int main(void) {
+	unsigned char pass[] = "test";
+	unsigned char salt[] = "1234";
+	unsigned char digest[HASH_DIGEST_SIZE_MD5], encoded_digest[(HASH_DIGEST_SIZE_MD5 * 2) + 1];
+	int rounds = 10;
+	size_t out_len = 0;
+
+	kdf_pbkdf2_md5(digest, pass, sizeof(pass) - 1, salt, sizeof(salt) - 1, rounds, HASH_DIGEST_SIZE_MD5);
+	encode_buffer_base16(encoded_digest, &out_len, digest, HASH_DIGEST_SIZE_MD5);
+
+	puts((char *) encoded_digest);
+
+	return 0;
+}
+
---
+++ example/kdf_pbkdf2_sha384.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <psec/encode.h>
+#include <psec/hash.h>
+#include <psec/kdf.h>
+#include <psec/mac.h>
+
+int main(void) {
+	unsigned char pass[] = "test";
+	unsigned char salt[] = "1234";
+	unsigned char digest[HASH_DIGEST_SIZE_SHA384], encoded_digest[(HASH_DIGEST_SIZE_SHA384 * 2) + 1];
+	int rounds = 10;
+	size_t out_len = 0;
+
+	kdf_pbkdf2_sha384(digest, pass, sizeof(pass) - 1, salt, sizeof(salt) - 1, rounds, HASH_DIGEST_SIZE_SHA384);
+	encode_buffer_base16(encoded_digest, &out_len, digest, HASH_DIGEST_SIZE_SHA384);
+
+	puts((char *) encoded_digest);
+
+	return 0;
+}
+
