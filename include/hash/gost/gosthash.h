1a1593a7bc2f408016a2bb261a7c6dcc9d2b6fd5
Pedro A. Hortas <pah@ucodev.org>
2014-09-05 19:41:12 +0100

    v0.06g

--- include/hash/gost/gosthash.h
+++ include/hash/gost/gosthash.h
@@ -18,9 +18,22 @@
  */
 
 /*
- * The original code (before libpsec changes) is in the public domain.
- *
- * It can be downloaded from:
+Copyright © 2014, Markku-Juhani O. Saarinen <mjos@iki.fi> All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* Neither Markku-Juhani O. Saarinen's name nor NTNU, Kudelski Security, STRIBOB, BLINKER, or the names of any other affiliated Companies, Institutions, Designs or Products may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MARKKU-JUHANI O. SAARINEN BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+While Kudelski Security and Norwegian University of Science and Technology (NTNU) have been acting as a sponsors of this initiative, Markku-Juhani O. Saarinen is the sole and exclusive owner of the copyright in this software at the exclusion of NTNU and Kudelski Security. NTNU or Kudelski Security have no responsibility whatsoever in such initiative and/or software and IN NO EVENT SHALL KUDELSKI SECURITY OR NTNU BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION).
+*/
+
+
+/*
+ * The original code can be downloaded from:
  *
  * http://www.autochthonous.org/crypto/gosthash.tar.gz
  *
