afdcd7349eace94065b82d07667e9809ee15c457
Pedro A. Hortas <pah@ucodev.org>
2015-02-10 01:12:39 +0000

    v0.07w3

--- src/auth/shadow/nop.c
+++ src/auth/shadow/nop.c
@@ -1,4 +1,12 @@
+#include <errno.h>
+
 void no_auth_shadow_support(void) {
 	return;
+}
+
+int shadow_user_pass_verify(const char *username, const char *password) {
+	errno = ENOSYS;
+
+	return -1;
 }
 
--- src/ke/dh/nop.c
+++ src/ke/dh/nop.c
@@ -1,4 +1,36 @@
+#include <stdio.h>
+#include <errno.h>
+
 void no_dh_support(void) {
 	return;
+}
+
+unsigned char *dh_init_private_key(unsigned char *priv, size_t priv_size) {
+	errno = ENOSYS;
+
+	return NULL;
+}
+
+unsigned char *dh_compute_public_key(
+	unsigned char *pub,
+	size_t pub_size,
+	const unsigned char *priv,
+	size_t priv_size)
+{
+	errno = ENOSYS;
+
+	return NULL;
+}
+	
+unsigned char *dh_compute_shared_key(
+	unsigned char *shared,
+	const unsigned char *pub,
+	size_t pub_size,
+	const unsigned char *priv,
+	size_t priv_size)
+{
+	errno = ENOSYS;
+
+	return NULL;
 }
 
